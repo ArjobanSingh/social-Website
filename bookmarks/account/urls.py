57bf7ab6e695948f607e518e38ff1782c9841105
ArjobanSingh <arjobansingh1998@gmail.com>
2019-10-23 00:31:38 +0530

    registration form added

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -10,13 +10,14 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
 
     path('', views.dashboard, name='dashboard'),
-    
+
     path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),name="password_change_done"),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('register/', views.register, name='register'),
 
     #alternate way to add all authentication views(including loginand logout, password change and reset, just add following line)
     #path('', include('django.contrib.auth.urls')),
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -26,6 +26,23 @@ def user_login(request):
         form = LoginForm()
         return render(request, "account/login.html", {"form": form})  
 '''
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            #create new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+
+            cd = user_form.cleaned_data
+            #set the chosen password
+            new_user.set_password(cd['password']) # new_user.set_password(user_form.cleaned_data['password'])
+            #save the user
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, "account/register.html", {"user_form": user_form})
 
 @login_required
 def dashboard(request):
