6f943ca9f14822e3c5d179780fd96b8053ded295
ArjobanSingh <arjobansingh1998@gmail.com>
2019-10-24 00:50:13 +0530

    profile done

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -18,16 +18,16 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse("Authenticated Successfuly")
-                else:
                     return redirect('dashboard')
+                else:
+                    return render(request, "account/login.html", {"form": form, "errors":True})  
             else:
-                return HttpResponse("Invalid Login")
+                return render(request, "account/login.html", {"form": form, "errors":True})  
     else:
         if request.user.is_authenticated:
             return redirect('dashboard')
         form = LoginForm()
-        return render(request, "account/login.html", {"form": form})  
+        return render(request, "account/login.html", {"form": form, "errors":False})  
 
 
 def register(request):
@@ -59,7 +59,7 @@ def edit(request):
     if request.method == "POST":
         user_form = UserEditForm(instance=request.user, data=request.POST)
 
-        profile_form = ProfileEditForm(instance=request.user.Profile,data=request.POST, files=request.FILES)
+        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST, files=request.FILES)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -68,4 +68,4 @@ def edit(request):
     else:
         user_form=UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})        
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})        
